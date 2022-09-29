2e8d77b2d1a52c2bbba60f72431a35554361756a
moon <anstnqls554@gmail.com>
2022-09-29 11:05:32 +0900

    안내상자 추가

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -43,6 +43,7 @@ import { ref, computed } from "vue";
 import axios from "axios";
 import _ from "lodash";
 import ToastBox from "@/components/ToastBox.vue";
+import { useToast } from "@/composables/toast.js";
 export default {
   components: {
     ToastBox,
@@ -50,6 +51,40 @@ export default {
   setup() {
     const route = useRoute();
     const router = useRouter();
+
+    // 생명 주기 코드(LifeCycle Hooks)
+    //  화면에 보여지기전(등록) 단계
+
+    // : 화면을 보여주기 전에 데이터를 준비하는 Hook
+    // onBeforeMount(() => {
+    //   console.log("onBeforeMount");
+    //   console.log(document.querySelector("#test"));
+    // });
+
+    // 화면에 보여지는 단계
+    // onMounted(() => {
+    //   console.log("onMounted");
+    //   console.log(document.querySelector("#test"));
+    // });
+    // 화면 갱신이 되기 전 단계
+    // onBeforeUpdate(() => {
+    //   console.log("onBeforeUpdate");
+    // });
+    // 화면 갱신이 되고 난 후
+    // onUpdated(() => {
+    //   console.log("onUpdated");
+    // });
+    // 컴포넌트가 화면에서 제거되기 전 준비 단계
+    // : 메모리를 정리 하는곳
+    // onBeforeUnmount(() => {
+    //   console.log("onBeforemount");
+    // });
+    // 컴포넌트가 완전히 제거 되었을 때
+    // onUnmounted(() => {
+    //   clearTimeout(toastTimer.value);
+    //   // console.log("onUnmounted");
+    // });
+
     // 데이터 로딩 화면상태
     const loading = ref(true);
     const todo = ref(null);
@@ -103,19 +138,9 @@ export default {
     const todoState = computed(() => {
       return _.isEqual(todo.value, originalTodo.value);
     });
-    const toastMessage = ref("");
-    const toastType = ref("");
-    const showToast = ref(false);
-    const triggerToast = (message, color = "success") => {
-      toastMessage.value = message;
-      toastType.value = color;
-      showToast.value = true;
-      setTimeout(() => {
-        toastMessage.value = "";
-        toastType.value = "";
-        showToast.value = false;
-      }, 3000);
-    };
+    // 안내창 관련
+    const { showToast, toastMessage, toastType, triggerToast } = useToast();
+
     return {
       todo,
       loading,
